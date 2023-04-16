79d5d734ed521d2973bc3b97c6435286ee91d7c1
sunnyheee <fool0116@naver.com>
2023-04-17 00:04:24 +0900

    [useStatus, useCallback] Login check

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -1,8 +1,13 @@
 import PropsTypes from "prop-types";
 import Link from "next/Link";
-import { Menu } from "antd";
+import { Menu, Input, Row, Col } from "antd";
+import { useState } from "react";
+import LoginForm from "../components/LoginForm";
+import UserProfile from "../components/UserProfile";
 
 const AppLayout = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div>
       <Menu mode="horizontal">
@@ -17,11 +22,25 @@ const AppLayout = ({ children }) => {
           </Link>
         </Menu.Item>
         <Menu.Item>
+          <Input.Search enterButton style={{ verticalAlign: "middle" }} />
+        </Menu.Item>
+        <Menu.Item>
           <Link href="/signup">
             <a>SIGNUP</a>
           </Link>
         </Menu.Item>
       </Menu>
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          {isLoggedIn ? <UserProfile /> : <LoginForm />}
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a href="https://github.com/sunnyheee">Made by sunny</a>
+        </Col>
+      </Row>
       {children}
     </div>
   );
---
+++ components/LoginForm.js
@@ -0,0 +1,49 @@
+import { useCallback, useState } from "react";
+import { Form, Input, Button } from "antd";
+import Link from "next/link";
+
+const LoginForm = () => {
+  const [id, setId] = useState("");
+  const [password, setpassword] = useState("");
+
+  const onChangeId = useCallback((e) => {
+    setId(e.target.value);
+  }, []);
+
+  const onChangePassword = useCallback((e) => {
+    setpassword(e.target.value);
+  }, []);
+
+  return (
+    <Form>
+      <div>
+        <label htmlFor="user-id">ID</label>
+        <br />
+        <Input name="user-id" value={id} onChange={onChangeId} required />
+      </div>
+      <div>
+        <label htmlFor="user-password">PW</label>
+        <br />
+        <Input
+          name="user-password"
+          type="password"
+          value={password}
+          onChange={onChangePassword}
+          required
+        />
+      </div>
+      <div>
+        <Button type="primary" htmlType="submit" loading={false}>
+          Login
+        </Button>
+        <Link href="/signuo">
+          <a>
+            <Button>JOIN</Button>
+          </a>
+        </Link>
+      </div>
+    </Form>
+  );
+};
+
+export default LoginForm;
